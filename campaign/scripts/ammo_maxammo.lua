886b40538b77ea2df081f4a5730cf3fd3f8d4448
Wil Thieme <wil.thieme@protonmail.com>
2021-08-15 20:33:29 -0400

    refill quiver from inventory on double click

--- campaign/scripts/ammo_maxammo.lua
+++ campaign/scripts/ammo_maxammo.lua
@@ -11,3 +11,29 @@ function onValueChanged()
 		window.switchAmmo(window.type.getValue() == 1)
 	end
 end
+
+function onDoubleClick(x, y)
+	if super and super.onDoubleClick then
+		super.onDoubleClick(x, y);
+	end
+
+	local nodeWeapon = window.getDatabaseNode();
+	local rActor = ActorManager.resolveActor(nodeWeapon.getChild('...'));
+	local nodeAmmo = AmmunitionManager.getAmmoNode(nodeWeapon, rActor);
+
+	if nodeAmmo then
+		local nCount = DB.getValue(nodeAmmo, count[1], 0)
+		local nAmmo = DB.getValue(nodeWeapon, ammo[1], 0)
+
+		if (nAmmo > 0) and (nCount > 0) then
+			local nReload = (nCount - nAmmo)
+			if nReload > 0 then
+				DB.setValue(nodeWeapon, ammo[1], 'number', 0)
+				DB.setValue(nodeAmmo, count[1], 'number', nReload)
+			else
+				DB.setValue(nodeWeapon, ammo[1], 'number', nAmmo - nCount)
+				DB.setValue(nodeAmmo, count[1], 'number', 0)
+			end
+		end
+	end
+end
