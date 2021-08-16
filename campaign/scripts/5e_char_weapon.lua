9ade5e5533d7dfe65aaf780c04b8b60172510eff
Wil Thieme <wil.thieme@protonmail.com>
2021-08-16 08:42:29 -0400

    5e attack limit automation

---
+++ campaign/scripts/5e_char_weapon.lua
@@ -0,0 +1,24 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onAttackAction(draginfo)
+	local nodeWeapon = getDatabaseNode();
+	local nodeChar = nodeWeapon.getChild("...")
+
+	-- Build basic attack action record
+	local rAction = CharWeaponManager.buildAttackAction(nodeChar, nodeWeapon);
+	
+	-- Decrement ammo
+	CharWeaponManager.decrementAmmo(nodeChar, nodeWeapon);
+	
+	-- Perform action
+	local rActor = ActorManager.resolveActor(nodeChar);
+	local nMaxAmmo = DB.getValue(nodeWeapon, 'maxammo', 0)
+	local nMaxAttacks = nMaxAmmo - DB.getValue(nodeWeapon, 'ammo', 0)
+	if not (nMaxAmmo > 0) or (nMaxAttacks >= 1) then	
+		ActionAttack.performRoll(draginfo, rActor, rAction);
+		return true;
+	end
+end
