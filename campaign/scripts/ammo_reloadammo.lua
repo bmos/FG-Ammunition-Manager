d45c7e9edf618d088f6a06842904554a35e6515b
Wil Thieme <wil.thieme@protonmail.com>
2021-09-20 10:22:12 -0400

    Update ammo_reloadammo.lua

--- campaign/scripts/ammo_reloadammo.lua
+++ campaign/scripts/ammo_reloadammo.lua
@@ -17,10 +17,10 @@ function onClickRelease(target, button, image, ...)
 		local nAmmo = DB.getValue(nodeWeapon, ammo[1], 0)
 
 		if (nAmmo > 0) and (nCount > 0) then
-			local nReload = (nCount - nAmmo)
-			if nReload > -1 then
+			local nAmmoRemaining = (nCount - nAmmo)
+			if nAmmoRemaining > -1 then
 				DB.setValue(nodeWeapon, ammo[1], 'number', 0)
-				DB.setValue(nodeAmmo, count[1], 'number', nReload)
+				DB.setValue(nodeAmmo, count[1], 'number', nAmmoRemaining)
 				ChatManager.Message(Interface.getString("char_actions_reload_full"), true, rActor);
 			else
 				DB.setValue(nodeWeapon, ammo[1], 'number', nAmmo - nCount)
