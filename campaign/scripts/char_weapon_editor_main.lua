1c1cefff344d56cce62d3d5583bf311f8345c119
Wil Thieme <wil.thieme@protonmail.com>
2022-02-15 15:23:09 -0500

    ammo details visibility limits
    don't show ammo on weapon details page if weapon isn't ranged.

---
+++ campaign/scripts/char_weapon_editor_main.lua
@@ -0,0 +1,38 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onInit()
+	if super and super.onInit then
+		super.onInit()
+	end
+	local sNode = getDatabaseNode().getPath();
+	DB.addHandler(sNode, "onChildUpdate", onDataChanged);
+	onDataChanged();
+end
+
+function onClose()
+	if super and super.onClose then
+		super.onClose()
+	end
+	local sNode = getDatabaseNode().getPath();
+	DB.removeHandler(sNode, "onChildUpdate", onDataChanged);
+end
+
+function onDataChanged()	
+	if super and super.onDataChanged then
+		super.onDataChanged()
+	end
+	local nodeWeapon = getDatabaseNode();
+	local bRanged = DB.getValue(nodeWeapon, 'type', 0) == 1;
+	header_ammo.setVisible(bRanged);
+	ammopicker.setVisible(bRanged);
+	label_ammopicker.setVisible(bRanged);
+	recoverypercentage.setVisible(bRanged);
+	label_recoverypercentage.setVisible(bRanged);
+	label_ammopercentof.setVisible(bRanged);
+	missedshots.setVisible(bRanged);
+	label_missedshots.setVisible(bRanged);
+	recoverammo.setVisible(bRanged);
+end
