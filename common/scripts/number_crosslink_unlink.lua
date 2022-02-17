7ad6189e550edbb4022291f89d81107570ae4277
Wil Thieme <wil.thieme@protonmail.com>
2022-02-17 15:50:58 -0500

    unlinking improvements

---
+++ common/scripts/number_crosslink_unlink.lua
@@ -0,0 +1,104 @@
+-- 
+-- Please see the LICENSE.md file included with this distribution for 
+-- attribution and copyright information.
+--
+
+local bLocked = false;
+local sLink = nil;
+local widget = nil;
+local originalvalue = 0;
+
+function onInit()
+	if super and super.onInit then
+		super.onInit();
+	end
+
+	if self.update then
+		self.update();
+	end
+
+	onLinkUpdated()
+end
+
+function onClose()
+	if sLink then
+		DB.removeHandler(sLink, "onUpdate", onLinkUpdated);
+	end
+end
+
+function onDrop(x, y, draginfo)
+	if Session.IsHost then
+		if draginfo.getType() ~= "number" then
+			return false;
+		end
+
+		if self.handleDrop then
+			self.handleDrop(draginfo);
+			return true;
+		end
+	end
+end
+
+function onValueChanged()
+	if sLink then
+		if not bLocked then
+			bLocked = true;
+
+			if sLink and not isReadOnly() then
+				DB.setValue(sLink, "number", getValue());
+			end
+
+			if self.update then
+				self.update();
+			end
+
+			bLocked = false;
+		end
+	else
+		if self.update then
+			self.update();
+		end
+	end
+end
+
+function onLinkUpdated()
+	if sLink and not bLocked then
+		bLocked = true;
+
+		setValue(DB.getValue(sLink, 0));
+		
+		if self.update then
+			self.update();
+		end
+
+		bLocked = false;
+	end
+end
+
+function setLink(dbnode, bLock)
+	if sLink then
+		DB.removeHandler(sLink, "onUpdate", onLinkUpdated);
+		sLink = nil;
+		widget.destroy()
+		if not dbnode then setValue(originalvalue) end
+	else
+		originalvalue = getValue() or 0
+	end
+		
+	if dbnode then
+		sLink = dbnode.getPath();
+
+		if not nolinkwidget then
+			widget = addBitmapWidget("field_linked");
+			widget.setPosition("bottomright", 0, -2);
+		end
+		
+		if bLock == true then
+			setReadOnly(true);
+		end
+
+		DB.addHandler(sLink, "onUpdate", onLinkUpdated);
+		onLinkUpdated();
+	end
+end
+
