6b4742c39a23b9e2e54721cf89fe02f4b56a3253
SoxMax <irishsoxmax@gmail.com>
2022-06-07 18:11:12 -0400

    inital linked ammo starfinder

--- campaign/scripts/ammo_sfrpg_reload.lua
+++ campaign/scripts/ammo_sfrpg_reload.lua
@@ -54,3 +54,89 @@ function onReloadAction()
 
 	return true;
 end
+
+--	luacheck: globals hasLoadAction
+function hasLoadAction(nodeWeapon)
+	local bHasLoadAction
+	--	luacheck: globals type
+	local bRanged = (type.getValue() == 1);
+	local sWeaponName = string.lower(DB.getValue(nodeWeapon, 'name', 'ranged weapon'));
+	for _,v in pairs(AmmunitionManager.tLoadWeapons) do
+		if string.find(sWeaponName, v) then bHasLoadAction = true; break; end
+	end
+
+	return (bRanged and bHasLoadAction)
+end
+
+--	luacheck: globals automateAmmo
+function automateAmmo(nodeWeapon)
+	local bNotLoaded = (DB.getValue(nodeWeapon, 'isloaded') == 0);
+	DB.setValue(nodeWeapon, 'isloaded', 'number', 0);
+	if hasLoadAction(nodeWeapon) and bNotLoaded then
+		local rActor = ActorManager.resolveActor(nodeWeapon.getChild('...'));
+		local sWeaponName = string.lower(DB.getValue(nodeWeapon, 'name', 'ranged weapon'));
+
+		ChatManager.Message(string.format(Interface.getString('char_actions_notloaded'), sWeaponName), true, rActor);
+		return true;
+	end
+end
+
+function onDataChanged()
+	super.onLinkChanged();
+	super.onDamageChanged();
+
+	local nodeWeapon = getDatabaseNode();
+	local nodeAmmoLink = AmmunitionManager.getAmmoNode(nodeWeapon);
+	local rActor = ActorManager.resolveActor(nodeWeapon.getChild('...'));
+	local _, bInfiniteAmmo = AmmunitionManager.getAmmoRemaining(rActor, nodeWeapon, nodeAmmoLink);
+
+	--	luacheck: globals type
+	local bRanged = (type.getValue() == 1);
+	label_range.setVisible(bRanged);
+	rangeincrement.setVisible(bRanged);
+	-- isloaded.setVisible(bRanged and hasLoadAction(nodeWeapon));
+	label_ammo.setVisible(bRanged);
+	uses.setVisible(bRanged);
+	ammocounter.setVisible(bRanged and not bInfiniteAmmo and not nodeAmmoLink);
+
+	local sSpecial = DB.getValue(nodeWeapon, "special",""):lower();
+	if string.find(sSpecial, "unwieldy") then
+		bNoFull = true
+	elseif string.find(sSpecial, "explode") then
+		bNoFull = true
+	elseif string.find(sSpecial, "thrown") and bRanged then
+		bNoFull = true
+	else
+		bNoFull = false
+	end
+
+	if bNoFull then
+		attack1.setVisible(false);
+	end
+
+	
+
+	if uses.setLink then
+		if nodeAmmoLink then
+			uses.setLink(nodeAmmoLink.getChild('count'))
+		else
+			uses.setLink()
+		end
+	else
+		Debug.chat("WARNING: NO AMMUNITION SET ON ITEM", DB.getValue(nodeWeapon, 'name'))
+	end
+end
+
+function onInit()
+	super.registerMenuItem(Interface.getString("menu_deleteweapon"), "delete", 4);
+	super.registerMenuItem(Interface.getString("list_menu_deleteconfirm"), "delete", 4, 3);
+
+	local sNode = getDatabaseNode().getPath();
+	DB.addHandler(sNode, "onChildUpdate", onDataChanged);
+	onDataChanged();
+end
+
+function onClose()
+	local sNode = getDatabaseNode().getPath();
+	DB.removeHandler(sNode, "onChildUpdate", onDataChanged);
+end
