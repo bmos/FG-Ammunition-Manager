d89ba9915cc156c162f4cc63823d8593c1b058a4
SoxMax <irishsoxmax@gmail.com>
2022-06-09 21:41:23 -0400

    remove dead code

--- campaign/scripts/sfrpg_current_ammo.lua
+++ campaign/scripts/sfrpg_current_ammo.lua
@@ -5,17 +5,3 @@ function onDoubleClick(x,y)
     local rActor, _ = CharManager.getWeaponAttackRollStructures(nodeWeapon);
     ChatManager.Message(Interface.getString("char_message_reloadammo"), true, rActor)
 end
-
--- function onValueChanged()
---     if super and super.onValueChanged then
---         super.onValueChanged()
---     end
---     local nodeWeapon = window.getDatabaseNode()
--- 	local nodeLinkedWeapon = AmmunitionManager.getShortcutNode(nodeWeapon, 'shortcut')
--- 	local usage = DB.getValue(nodeLinkedWeapon, 'usage', 1)
--- 	local uses = DB.getValue(nodeWeapon, 'uses', 1)
--- 	local currentAmmo = getValue()
--- 	local ammoUsed = math.max(0, uses - math.floor(currentAmmo / usage))
-
--- 	DB.setValue(nodeWeapon, 'ammo', 'number', ammoUsed)
--- end
