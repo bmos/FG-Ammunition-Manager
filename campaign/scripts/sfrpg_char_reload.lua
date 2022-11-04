e63aa15a28728d0380ccd071ce8fde6bbfcdae42
SoxMax <irishsoxmax@gmail.com>
2022-11-04 15:16:15 -0400

    update the reload code

--- campaign/scripts/sfrpg_char_reload.lua
+++ campaign/scripts/sfrpg_char_reload.lua
@@ -29,59 +29,73 @@ local function getContainedItems(nodeContainer)
 	return containedItems
 end
 
-local function moveInventoryAmmunition(weaponActionNode, newAmmunitionNode)
+local function loadCartridges(weaponActionNode, newAmmoNode, loadedAmmoNode)
 	local weaponInventoryNode = AmmunitionManager.getShortcutNode(weaponActionNode, 'shortcut')
 	local maxAmmo, ammoType = parseWeaponCapacity(DB.getValue(weaponInventoryNode, 'capacity', ''))
-	if ammoType == 'drawn' then
-		return newAmmunitionNode
+	local currentAmmoCount = 0
+	if loadedAmmoNode then
+		currentAmmoCount = DB.getValue(loadedAmmoNode, 'count', 0)
+	else
+		loadedAmmoNode = newAmmoNode.getParent().createChild()
+		loadedAmmoNode = DB.copyNode(newAmmoNode, loadedAmmoNode)
+		DB.setValue(loadedAmmoNode, 'location', 'string', ItemManager.getDisplayName(weaponInventoryNode, true))
 	end
+	local newAmmoCount = DB.getValue(newAmmoNode, 'count', 0)
+	local ammoNeeded = maxAmmo - currentAmmoCount
+	if ammoNeeded > newAmmoCount then
+		ammoNeeded = newAmmoCount
+	end
+	DB.setValue(loadedAmmoNode, 'count', 'number', currentAmmoCount + ammoNeeded)
+	DB.setValue(newAmmoNode, 'count', 'number', newAmmoCount - ammoNeeded)
+	return loadedAmmoNode
+end
+
+local function isSameAmmo(ammo1, ammo2)
+	return ammo1 and ammo2 and ItemManager.compareFields(ammo1, ammo2, true)
+end
+
+local function unloadAmmunition(loadedAmmoNode)
+	if loadedAmmoNode then
+		DB.setValue(loadedAmmoNode, 'location', 'string', '')
+	end
+end
+
+local function moveInventoryAmmunition(weaponActionNode, newAmmoNode)
+	local weaponInventoryNode = AmmunitionManager.getShortcutNode(weaponActionNode, 'shortcut')
 	local loadedAmmoNode = getContainedItems(weaponInventoryNode)[1]
-	local weaponName = ItemManager.getDisplayName(weaponInventoryNode, true)
-	if loadedAmmoNode then --weapon has ammo still in it
-		if newAmmunitionNode then
-			if ammoType == 'charges' then
-				DB.setValue(loadedAmmoNode, 'location', 'string', '')
-				DB.setValue(newAmmunitionNode, 'location', 'string', weaponName)
-				return newAmmunitionNode
-			else
-			local newAmmoCount = DB.getValue(newAmmunitionNode, 'count', 0)
-				local currentCount = DB.getValue(loadedAmmoNode, 'count', 0)
-				local ammoNeeded = maxAmmo - currentCount
-				if newAmmoCount <= ammoNeeded then
-					DB.setValue(loadedAmmoNode, 'count', 'number', newAmmoCount)
-					DB.deleteNode(newAmmunitionNode)
-					return loadedAmmoNode
-				else
-					DB.setValue(loadedAmmoNode, 'count', 'number', maxAmmo)
-					DB.setValue(newAmmunitionNode, 'count', 'number', newAmmoCount - ammoNeeded)
-					return loadedAmmoNode
-				end
-			end
-		else -- unload ammo
+	if not newAmmoNode then -- no new ammo, unload old
+		unloadAmmunition(loadedAmmoNode)
+		return newAmmoNode
+	end
+
+	local maxAmmo, ammoType = parseWeaponCapacity(DB.getValue(weaponInventoryNode, 'capacity', ''))
+	if ammoType == 'drawn' then
+		return newAmmoNode
+	end
+	
+	if ammoType == 'charges' then
+		if loadedAmmoNode then
 			DB.setValue(loadedAmmoNode, 'location', 'string', '')
 		end
+		DB.setValue(newAmmoNode, 'location', 'string', ItemManager.getDisplayName(weaponInventoryNode, true))
+		return newAmmoNode
+	end
+	if isSameAmmo(loadedAmmoNode, newAmmoNode) then
+		return loadCartridges(weaponActionNode, newAmmoNode, loadedAmmoNode)
 	else
-		if newAmmoCount <= maxAmmo then
-			DB.setValue(newAmmunitionNode, 'location', 'string', weaponName)
-			return newAmmunitionNode
-		else
-			local clonedNode = newAmmunitionNode.getParent().createChild()
-			clonedNode = DB.copyNode(newAmmunitionNode, clonedNode)
-			DB.setValue(clonedNode, 'count', 'number', maxAmmo)
-			DB.setValue(newAmmunitionNode, 'count', 'number', newAmmoCount - maxAmmo)
-			return clonedNode
-		end
+		unloadAmmunition(loadedAmmoNode)
+		return loadCartridges(weaponActionNode, newAmmoNode)
 	end
 end
 
 function loadAmmo(ammoItem)
-	local nodeAmmoItem = ammoItem.getDatabaseNode()
+	local nodeWeaponAction = getDatabaseNode()
 	if ammoItem then
-		local nodeWeapon = getDatabaseNode()
-		local loadedAmmo = moveInventoryAmmunition(nodeWeapon, nodeAmmoItem)
-		DB.setValue(nodeWeapon, 'ammopicker', 'string', ItemManager.getDisplayName(nodeAmmoItem, true))
-		DB.setValue(nodeWeapon, 'ammoshortcut', 'windowreference', 'item', '....inventorylist.' .. loadedAmmo.getName())
-		local rActor = CharManager.getWeaponAttackRollStructures(nodeWeapon)
+		local nodeAmmoItem = ammoItem.getDatabaseNode()
+		local loadedAmmo = moveInventoryAmmunition(nodeWeaponAction, nodeAmmoItem)
+		DB.setValue(nodeWeaponAction, 'ammopicker', 'string', ItemManager.getDisplayName(nodeAmmoItem, true))
+		DB.setValue(nodeWeaponAction, 'ammoshortcut', 'windowreference', 'item', '....inventorylist.' .. loadedAmmo.getName())
+		local rActor = CharManager.getWeaponAttackRollStructures(nodeWeaponAction)
 		local messagedata = {
 			text = Interface.getString('char_message_reloadammo'),
 			sender = rActor.sName,
@@ -89,8 +103,9 @@ function loadAmmo(ammoItem)
 		}
 		Comm.deliverChatMessage(messagedata)
 	else
-		DB.setValue(nodeWeapon, 'ammopicker', 'string', '')
-		DB.setValue(nodeWeapon, 'ammoshortcut', 'windowreference', 'item', '')
+		moveInventoryAmmunition(nodeWeaponAction)
+		DB.setValue(nodeWeaponAction, 'ammopicker', 'string', '')
+		DB.setValue(nodeWeaponAction, 'ammoshortcut', 'windowreference', 'item', '')
 	end
 
 	parentcontrol.window.close()
